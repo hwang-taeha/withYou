df0ce5b4c6f8d08cb6b0a20e454b0915249e9ffe
황태하 <hwangtaeha@MacBook-Pro.local>
2020-11-03 17:41:49 +0900

    :scissors:           edit    :changed typescript

---
+++ with-u/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a>Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
